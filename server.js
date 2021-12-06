a47061789563a7023b7b12831aaa582da9e29c06
nmkrishna <murali@digitaldots.ai>
2021-12-07 00:02:43 +0530

    changed root

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-//Install express server
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const app = express();
+app.use(requireHTTPS);
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist'));
+app.use(express.static(’./dist/jj-chart’));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/'}),
+app.get(’/*’, function(req, res) {
+  res.sendFile(’index.html’, {root: 'dist/jj-chart/’}
 );
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
