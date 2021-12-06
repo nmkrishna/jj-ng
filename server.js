73048dfb855395357ce401e9571b5559f8fa1112
nmkrishna <murali@digitaldots.ai>
2021-12-06 23:44:17 +0530

    server js chnaged

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/jj-ng'));
+app.use(express.static('./dist'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/jj-ng/'}),
+    res.sendFile('index.html', {root: 'dist/'}),
 );
 
 // Start the app by listening on the default Heroku port
